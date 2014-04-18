6a80c27fb2e4c636dd69521e973a5867ecb4e017
Alex Rudy <alex.rudy@gmail.com>
2014-04-18 15:50:21 -0700

    Tridiagonal definition file

--- Flox/tridiagonal/__init__.py
+++ Flox/tridiagonal/__init__.py
@@ -11,4 +11,8 @@ from __future__ import (absolute_import, unicode_literals, division, print_funct
 
 from ._tridiagonal import tridiagonal_solver, tridiagonal_from_matrix, tridiagonal_split_matrix
 
-__all__ = ['tridiagonal_solver', 'tridiagonal_from_matrix', 'tridiagonal_split_matrix']
+def tridiagonal_combine_matrix(sub, dia, sup):
+    """Combine a tridiagonal matrix."""
+    return np.matrix(np.diag(sub, -1) + np.diag(dia, 0) + np.diag(sup, 1))
+
+__all__ = ['tridiagonal_solver', 'tridiagonal_from_matrix', 'tridiagonal_split_matrix', 'tridiagonal_combine_matrix']
