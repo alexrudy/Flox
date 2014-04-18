7a01e5e7bff5db9f6303c825e615cde1f012d631
Alex Rudy <alex.rudy@gmail.com>
2014-04-18 10:45:49 -0700

    Improved performance tests

--- performance/tridiagonal.py
+++ performance/tridiagonal.py
@@ -22,7 +22,7 @@ def tridiagonal_trial_setup(n, seed):
     from Flox.tridiagonal import tridiagonal_from_matrix
     from Flox.tridiagonal.test_tridiagonal import assemble_tridiagonal_matrix, assemble_solution_matrix
     n = int(n)
-    eps = 1e-7
+    eps = 1e-3
     
     mat = assemble_tridiagonal_matrix(n, eps, seed=seed)
     sol = assemble_solution_matrix(n, seed=seed)
@@ -32,12 +32,12 @@ def tridiagonal_trial_setup(n, seed):
     
     return rhs, res, mat
     
-def tridiagonal_split_trial_setup():
+def tridiagonal_split_trial_setup(n, seed):
     """Test pre-split arrays."""
     from Flox.tridiagonal import tridiagonal_split_matrix
     from Flox.tridiagonal.test_tridiagonal import assemble_tridiagonal_matrix, assemble_solution_matrix
     n = int(n)
-    eps = 1e-7
+    eps = 1e-3
     
     mat = assemble_tridiagonal_matrix(n, eps, seed=seed)
     sol = assemble_solution_matrix(n, seed=seed)
@@ -65,20 +65,20 @@ def tridiagonal_split_trial_setup_cmd(n, seed):
     return textwrap.dedent("""
     from __main__ import tridiagonal_split_trial_setup
     from Flox.tridiagonal import tridiagonal_solver
-    rhs, res, sup, dia, sub = tridiagonal_trial_setup({n:d},{seed:d})    
+    rhs, res, sup, dia, sub = tridiagonal_split_trial_setup({n:d},{seed:d})    
     """.format(n=n,seed=seed))
 
 def tridiagonal_trial(n, seed):
     """Tridiagonal trial."""
     cmd = "status = tridiagonal_from_matrix(rhs, res, mat)"
     print("Trying n={:d}".format(n))
-    return timeit.timeit(cmd, setup=tridiagonal_trial_setup_cmd(n, seed), number=int(1e5))
+    return timeit.timeit(cmd, setup=tridiagonal_trial_setup_cmd(n, seed), number=int(1e6))
 
 def tridiagonal_split_trial(n, seed):
     """Tridiagonal trial."""
     cmd = "status = tridiagonal_solver(rhs, res, sub, dia, sup)"
     print("Trying n={:d}".format(n))
-    return timeit.timeit(cmd, setup=tridiagonal_split_trial_setup_cmd(n, seed), number=int(1e4))
+    return timeit.timeit(cmd, setup=tridiagonal_split_trial_setup_cmd(n, seed), number=int(1e6))
 
 
 def tridiagonal_timing(ns, seed):
@@ -89,15 +89,20 @@ if __name__ == '__main__':
     
     filename = os.path.join(os.path.dirname(__file__),"tridiagonal.npy")
     if os.path.exists(filename):
-        ns, times, times_split = np.vsplit(np.load(filename))
+        ns, times, times_split = np.hsplit(np.load(filename).T, 3)
     else:
         ns = np.power(2, np.arange(1,10))
-        times = tridiagonal_timing(ns, 5)
         times_split = [tridiagonal_split_trial(n, 5) for n in ns]
+        times = tridiagonal_timing(ns, 5)
         np.save(filename, np.vstack((ns, times, times_split)))
     
+    print("Plotting Timing Results")
     plotname = os.path.join(os.path.dirname(__file__),"tridiagonal.pdf")
-    plt.plot(ns, times, 'bo-')
-    plt.plot(ns, times_split, 'go-')
-    plt.savefig(filename)
+    plt.plot(ns, times, 'bo-', label="With Matrix Split")
+    plt.plot(ns, times_split, 'go-', label="Pre-split arrays")
+    plt.xlabel(r"Matrix Size $(n \times n)$")
+    plt.ylabel(r"Time per loop $(\mu s)$")
+    plt.title(r"Tridiagonal Algorithm Timing")
+    plt.legend(loc="upper left")
+    plt.savefig(plotname)
     
