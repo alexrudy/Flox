8b05060749dea4d6ddd3d71c4bd379fdfe9e13c0
Alex Rudy <alex.rudy@gmail.com>
2014-04-24 18:04:32 -0700

    Better handling of current time

--- Flox/linear/__init__.py
+++ Flox/linear/__init__.py
@@ -9,6 +9,8 @@
 
 from __future__ import (absolute_import, unicode_literals, division, print_function)
 
+import astropy.units as u
+from astropy.utils.console import ProgressBar
 from ._linear import LinearEvolver as _LinearEvolver
 
 class LinearEvolver(_LinearEvolver):
@@ -23,6 +25,21 @@ class LinearEvolver(_LinearEvolver):
         except:
             return super(LinearEvolver, self).__repr__()
         
+    def evolve_many(self, grids, total_time, chunksize=int(1e3)):
+        """Evolve over many iterations with a given total time."""
+        self.update_from_grids(grids)
+        tstart = grids.Time[grids.it]
+        with grids.in_nondimensional():
+            nd_total_time = self.time + total_time.to(grids.Time.unit).value
+        itotal = 0
+        with ProgressBar(grids.nt) as pb:
+            while (grids.Time[grids.it] - tstart) < total_time:
+                self.evolve(nd_total_time, chunksize)
+                self.to_grids(grids, grids.it+1)
+                itotal += 1
+                pb.update(itotal)
+        
+    
     @classmethod
     def from_grids(cls, grids):
         """Load the grid parameters into the LE"""
@@ -41,7 +58,9 @@ class LinearEvolver(_LinearEvolver):
         grids.it = iteration
         with grids.in_nondimensional():
             time_unit = grids.Time.unit
+            times = grids.Time
             self.get_state(grids.Temperature[...,grids.it], grids.Vorticity[...,grids.it])
-            grids.Time[grids.it] = self.time * time_unit
+            times[grids.it] = self.time * time_unit
+            grids.Time = times
         
             
