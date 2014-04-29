d183bd17f2d736ce30fb9dca19b9ea49cf787097
Alex Rudy <alex.rudy@gmail.com>
2014-04-29 10:09:01 -0700

    YAML tools write out nd-quantities as floats

--- Flox/input.py
+++ Flox/input.py
@@ -7,6 +7,8 @@
 #  Copyright 2014 Alexander Rudy. All rights reserved.
 # 
 
+from __future__ import (absolute_import, unicode_literals, division, print_function)
+
 from pyshell.yaml import PyshellLoader, PyshellDumper
 from pyshell.config import DottedConfiguration
 from pyshell.astron.yaml_tools import astropy_quantity_yaml_factory
@@ -15,6 +17,7 @@ import astropy.units
 
 from .array import ArrayEngine, ArrayYAMLSupport
 
+
 class FloxDumper(PyshellDumper):
     """A YAML Dumper which permits quantities!"""
     
@@ -22,6 +25,7 @@ class FloxDumper(PyshellDumper):
         """Initialize the dumper"""
         super(FloxDumper, self).__init__(*args, **kwargs)
         self.add_representer(astropy.constants.Constant, type(self).constant_representer)
+        self.add_representer(astropy.units.Quantity, type(self).astropy_representer)
         self.add_multi_representer(ArrayEngine, type(self).arrayengine_representer)
         self.add_multi_representer(ArrayYAMLSupport, type(self).arrayengine_representer)
     
@@ -31,12 +35,16 @@ class FloxDumper(PyshellDumper):
         
     def arrayengine_representer(self, data):
         """Represent an ArrayEngine object."""
-        print("Represent {}".format(ArrayEngine))
         params = {}
         params['()'] = ".".join([data.__class__.__module__,data.__class__.__name__])
         for param in data.get_parameter_list():
             params[param] = getattr(data, param)
         return self.represent_mapping('tag:yaml.org,2002:map',params)
+        
+    def astropy_representer(self, data):
+        if data.unit.decompose() == astropy.units.Unit(1):
+            return self.represent_scalar('tag:yaml.org,2002:float', "{!s}".format(float(data.value)))
+        return self.represent_scalar("!quantity", "{0.value} {0.unit:generic}".format(data))
         
 
 class FloxLoader(PyshellLoader):
@@ -46,13 +54,19 @@ class FloxLoader(PyshellLoader):
         """Initialize the dumper"""
         super(FloxLoader, self).__init__(*args, **kwargs)
         self.add_constructor("!constant", type(self).constant_constructor)
+        self.add_constructor("!quantity", type(self).astropy_constructor)
         
     def constant_constructor(self, node):
         """Construct an Astropy Constant"""
         scalar_value = self.construct_scalar(node).strip()
         return getattr(astropy.constants, scalar_value)
-
-astropy_quantity_yaml_factory(astropy.units.Quantity, FloxLoader, FloxDumper)
+        
+    def astropy_constructor(self, node):
+        scalar_value = self.construct_scalar(node)
+        parts = scalar_value.split(" ")
+        value = parts[0]
+        unit = " ".join(parts[1:])
+        return astropy.units.Quantity(float(value), unit=unit)
 
 class FloxConfiguration(DottedConfiguration):
     """A configuration object for Flox data."""
