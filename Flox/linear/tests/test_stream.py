Fixed stream function computation.

Flox/linear/tests/test_stream.py
@@ -29,7 +29,52 @@ def test_stream_solver():
     stream = w_stream(System)
     vorticity = w_vorticity(System, stream)
     s_vorticity = System.Vorticity
-    assert np.allclose(vorticity, s_vorticity)
+    assert np.allclose(vorticity[1:-1], s_vorticity[1:-1])
+    
+def test_stream_matrix():
+    """Matrix used for the stream solver function."""
+    from .._linear import StreamSolver
+    System = ConstantSystem(
+        dz = 0.8,
+        dt = 0.4,
+        a = 0.25,
+        nx = 3,
+        nz = 6,
+        Ra = 10.0,
+        Pr = 5.0,
+    )
+    
+    SS = StreamSolver(System.nz, System.nx)
+    SS.setup(System.dz, System.npa[0,:])
+    
+    sup, dia, sub = m_stream(System)
+    
+    assert sup.shape == SS.sup.shape
+    
+    assert np.allclose(sup, SS.sup)
+    
+    assert np.allclose(sub, SS.sub)
+    
+    assert np.allclose(dia, SS.dia)
+    
+    
+def m_stream(System):
+    """Extract the matrix components for Stream."""
+    sup = np.empty((System.nz, System.nx))
+    sup[...] = -1.0 / (System.dz)**2.0
+    dia = System.npa ** 2.0 + 2.0 / (System.dz) ** 2.0
+    sub = np.empty((System.nz, System.nx))
+    sub[...] = -1.0 / (System.dz)**2.0
+    
+    sub[0] = 0.0
+    sup[0] = 0.0
+    dia[0] = 1.0
+    dia[-1] = 1.0
+    sub[-1] = 0.0
+    sup[-1] = 0.0
+    
+    return sup, dia, sub
+    
     
 def dd_stream(System, stream):
     """Compute the second derivative of the stream function."""