a35c7f9ce38bde5ecdd53aa8f8b0bf10acea325f
Alex Rudy <alex.rudy@gmail.com>
2014-05-15 09:49:46 -0700

    Test base for initial conditions

---
+++ Flox/tests/test_ic.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# 
+#  test_ic.py
+#  EART275
+#  
+#  Created by Alexander Rudy on 2014-05-14.
+#  Copyright 2014 Alexander Rudy. All rights reserved.
+# 
+
+from __future__ import (absolute_import, unicode_literals, division, print_function)
+
+
+def test_stable_temp():
+    """Determine that the temperature is stable initially."""
+    pass
