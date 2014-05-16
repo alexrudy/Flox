c80df0a86665be5a43d5c1219f6e1100573a6a96
Alex Rudy <alex.rudy@gmail.com>
2014-05-16 16:12:35 -0700

    Initial condition object.

--- Flox/ic.py
+++ Flox/ic.py
@@ -13,6 +13,41 @@ from __future__ import (absolute_import, unicode_literals, division, print_funct
 
 import numpy as np
 
+class InitialConditioner(object):
+    """A class which builds up initial conditions."""
+    def __init__(self, params):
+        super(InitialConditioner, self).__init__()
+        self.params = params
+        
+    def run(self, System):
+        """Apply the initial conditions to a system."""
+        self.stable(System)
+        self.sin(System)
+        
+    def stable(self, System):
+        """Apply the stable perturbation."""
+        if self.params.get('stable',False):
+            stable_temperature_gradient(System)
+    
+    def sin(self, System):
+        """Sin perturbations in each mode."""
+        if self.params.get('sin.enable', False):
+            kmin, kmax = self.params.get('sin.k',[1,2])
+            lmin, lmax = self.params.get('sin.l',[1,2])
+            amplitude = self.params.get('sin.epsilon',0.5)
+            amp_mode = self.params.get('sin.amplitude','fixed')
+            for k in range(kmin, kmax):
+                for l in range(lmin, lmax):
+                    if amp_mode == 'fixed':
+                        eps = amplitude
+                    if amp_mode == 'random':
+                        eps = np.random.randn(1) * amplitude
+                    if amp_mode == 'powerlaw':
+                        eps = k**self.params.get('sin.powerlaw',-1) * amplitude
+                    if amp_mode == 'powerlaw-random':
+                        eps = np.random.randn(1) * k**self.params.get('sin.powerlaw',-1) * amplitude
+                    single_mode_perturbation(System, k, l, eps=eps)
+
 def z_array(System):
     """Z position array, appropriate for initialzing."""
     return (np.arange(System.nz + 2) / (System.nz + 1))[1:-1]
@@ -25,6 +60,10 @@ def stable_temperature_gradient(System):
         step = 1
     System.Temperature[:,0] = z_array(System)[::-1]
     
+
+def single_mode_perturbation(System, k, l=1, eps=1):
+    """Single mode perturbation."""
+    System.Temperature[:,k] = eps * np.sin(l * np.pi * z_array(System))
 
 def single_mode_linear_perturbation(System, mode=1, eps=1):
     """Make a standard mode for this perturbation."""
