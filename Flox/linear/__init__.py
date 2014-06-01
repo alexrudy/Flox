f46563b96dcac97fdb91016035de2811716881a7
Alex Rudy <alex.rudy@gmail.com>
2014-06-01 12:29:40 -0700

    Removing linear module

--- Flox/linear/__init__.py
+++ Flox/linear/__init__.py
@@ -9,28 +9,5 @@
 
 from __future__ import (absolute_import, unicode_literals, division, print_function)
 
-from ._linear import LinearEvolver as _LinearEvolver
-from ..evolver.base import Evolver
 
-class LinearEvolver(Evolver, _LinearEvolver):
-    """Linear evolver"""
-    def __init__(self, *args, **kwargs):
-        super(LinearEvolver, self).__init__()
-        
-    def get_packet_list(self):
-        """Variables"""
-        return [ "Temperature", "dTemperature", "Vorticity", "dVorticity", "Stream", "Time"]
-        
-    @classmethod
-    def from_system(cls, system, saftey=0.5):
-        """Load the grid parameters into the LE"""
-        ev = cls(
-            system.nz, system.nx,
-            system.nondimensionalize(system.npa).value,
-            system.nondimensionalize(system.dz).value, 
-            saftey
-            )
-        ev.Pr = system.nondimensionalize(system.Prandtl).value
-        ev.Ra = system.nondimensionalize(system.Rayleigh).value
-        return ev
     
