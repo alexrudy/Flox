c7721da8ceb94da5d4eed0e68e5b1deb4b532cb7
Alex Rudy <alex.rudy@gmail.com>
2014-06-01 12:30:19 -0700

    Removing old nonlinear evolver code

--- Flox/nonlinear/__init__.py
+++ Flox/nonlinear/__init__.py
@@ -9,34 +9,3 @@
 
 from __future__ import (absolute_import, unicode_literals, division, print_function)
 
-from ._nonlinear import NonlinearEvolver as _NonlinearEvolver
-from ..evolver.base import Evolver
-
-class NonlinearEvolver(_NonlinearEvolver, Evolver):
-    """Nonlinear evolver"""
-    def __init__(self, *args, **kwargs):
-        super(NonlinearEvolver, self).__init__()
-    
-    @classmethod
-    def get_parameter_list(cls):
-        """Retrieve the relevant parameters."""
-        return []
-    
-    def get_data_list(self):
-        """Variables"""
-        return [ "Temperature", "dTemperature", "Vorticity", "dVorticity", "Stream", "Time"]
-    
-    @classmethod
-    def from_system(cls, system, saftey=0.5):
-        """Load the grid parameters into the LE"""
-        ev = cls(
-            system.nz, system.nx,
-            system.nondimensionalize(system.npa).value,
-            system.nondimensionalize(system.dz).value,
-            system.nondimensionalize(system.aspect).value,
-            saftey
-            )
-        ev.Pr = system.nondimensionalize(system.Prandtl).value
-        ev.Ra = system.nondimensionalize(system.Rayleigh).value
-        return ev
-            
