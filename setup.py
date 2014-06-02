2ffed50d1df4541d5a734f27e23edf370a120827
Alex Rudy <alex.rudy@gmail.com>
2014-06-01 21:15:25 -0700

    Bumping version number.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ package_info['options'] = {
       },}
 setup(
     name = 'Flox',
-    version = "1.0.0",
+    version = "1.0.1",
     packages = find_packages(),
     install_requires = DEPENDENCIES,
     author = 'Alexander Rudy',
