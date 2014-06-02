2774910fb1474247543626a94949632303efc44a
Alex Rudy <alex.rudy@gmail.com>
2014-06-01 23:52:22 -0700

    Bugfix Release

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ package_info['options'] = {
 package_info['package_data'] = {'Flox': ['data/*','*.yml']}
 setup(
     name = 'Flox',
-    version = "1.1.0",
+    version = "1.1.1",
     packages = find_packages(),
     install_requires = DEPENDENCIES,
     author = 'Alexander Rudy',
